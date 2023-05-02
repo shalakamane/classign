50f74acc36e1a1befad47b6ae3425b7cf1a9f332
shalakamane <34000696+shalakamane@users.noreply.github.com>
2023-05-02 14:18:19 +0530

    Add files via upload

---
+++ exe1.py
@@ -0,0 +1,48 @@
+#1
+'''var1=int(input("Enter no1 :"))
+var2=int(input("Enter no2 :"))
+print("addition=",(var1+var2))
+print("square : ",var1**2)
+print("rasing no1: ",var1**var2)'''
+
+#2
+#string1=input("Enter String : ")
+#print(string1.upper())  
+string1=input("Enter String : ")
+print(string1.lower())  
+
+
+'''raise_salary_percentage=int(input("Enter salary raised percentage :"))
+Employee_name = "Gaurav"
+Existing_salary  = 900
+Raised_salary=Existing_salary +(Existing_salary * (raise_salary_percentage/100))
+print("Raising salary percentage of Gaurav is: " ,Raised_salary)
+
+my_dict = {"Employee_name" : "Gaurav" , "Raised_salary" : Raised_salary}
+print(my_dict)'''
+
+#4
+'''height=int(input("Enter height in cms : "))
+print(height)
+height_feet=height/2.54
+print("Height in feet : ",height_feet)
+
+height_inch=height * 0.39370  # or method
+print("Height in inches : ",height_inch)'''
+
+#5
+'''Amount=int(input("Enter amount in $$ : "))
+Amount_rupee=Amount * 81.74
+print("Amount in Rupee : ",Amount_rupee)'''
+
+#6
+'''Source=(input("Enter Source: "))
+Destination=(input("Enter Destination: "))
+Fare=int(input("Enter Fare: "))
+Discount_rate=int(input("Enter Discount_rate: "))
+print("fare from",Source,"to",Destination, "is", Fare,"INR with a discount of",Discount_rate,"%")'''
+
+
+
+
+
