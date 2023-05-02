a1d4e147b4938a481360c73755572a30dfb4685d
shalakamane <34000696+shalakamane@users.noreply.github.com>
2023-05-02 19:54:12 +0530

    Add files via upload

---
+++ exe2.py
@@ -0,0 +1,37 @@
+#1
+'''sum=0
+no=int(input("Enter a no : "))
+for i in range(0,no,1):
+    sum=sum+i
+    print("sum of numbers",sum)'''
+
+#2 prime number-
+'''num = int(input("Please, Enter the number: "))  
+  
+print ("The Prime Numbers are: ")  
+for number in range (0, num+1):  
+    if number > 1:  
+        for i in range (2, number):  
+            if (number % i) == 0:  
+                break  
+        else:  
+            print (number)  '''
+
+    
+#3 odd number
+num = int(input("Enter the number: "))
+for i in range(1, num+ 1):
+    if i % 2 != 0:
+        print("The odd numbers are : ")
+
+        print(i,end=" ")
+        
+#4 even no- 
+
+'''num = int(input("Enter the number: "))
+for i in range(1, num+ 1):
+    if i % 2 == 0:
+        print(i, end=" ")''' 
+    
+
+
